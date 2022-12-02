237b30763be64fb2b040f823e94cbfd4c5589346
TusharMorankar <119660791+TusharMorankar@users.noreply.github.com>
2022-12-02 14:39:31 +0530

    Add files via upload

---
+++ Program132.c
@@ -0,0 +1,17 @@
+//Accept full name from user and print to display
+//in this systax no full name display
+#include<stdio.h>
+#include<stdbool.h>
+
+
+int main()
+{
+    char Arr[50];
+
+    printf("Please Enter your full name\n");
+    scanf("%s",Arr);
+
+    printf("Your name is : %s\n",Arr);
+
+    return 0;
+}
---
+++ Program69.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int Arr[5];
+    int iCnt = 0;
+  
+    printf("Enter the elemets : ");
+    for(iCnt = 0; iCnt < 5; iCnt++)
